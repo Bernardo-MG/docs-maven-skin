802504e9c776eb4737f0d0a1d6c2624da68272a9
Bernardo Martínez Garrido <programming@wandrell.com>
2015-08-09 18:05:41 +0200

    Updated scripts.

--- .scripts/create-maven-settings.sh
+++ .scripts/create-maven-settings.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-# This script creates the Maven settings file
-# This includes the connection settings, which are loaded from the server's environment
+# This script creates the Maven settings file for deployment.
 #
-# The data generated during this script is sensible information. Don't print it on the console.
+# This includes the connection settings, which are loaded from the server's environment variables.
+#
+# The data generated during this script should not be shared. Don't print it on the console.
+#
+# The following environmental variables are used:
+# - DEPLOY_USER: string, user for the releases repo
+# - DEPLOY_PASSWORD: string, password for the releases repo
+# - DEPLOY_DEVELOP_USER: string, user for the development repo
+# - DEPLOY_DEVELOP_PASSWORD: string, password for the development repo
+# - DEPLOY_DOCS_USER: string, user for the releases documentation site repo
+# - DEPLOY_DOCS_PASSWORD: string, password for the releases documentation site repo
+# - DEPLOY_DOCS_DEVELOP_USER: string, user for the development documentation site repo
+# - DEPLOY_DOCS_DEVELOP_PASSWORD: string, password for the development documentation site repo
+# - SCM_BRANCH: string, the CMS branch from which the code has been taken
 
 {
    echo "<settings>";
@@ -11,32 +23,32 @@
    # Releases FTP
    echo "<server>";
       echo "<id>releases</id>";
-      echo "<username>\${env.REPO_RELEASES_USER}</username>";
-      echo "<password>\${env.REPO_RELEASES_PASSWORD}</password>";
+      echo "<username>\${env.DEPLOY_USER}</username>";
+      echo "<password>\${env.DEPLOY_PASSWORD}</password>";
    echo "</server>";
    # DEVELOP FTP
    echo "<server>";
       echo "<id>snapshots</id>";
-      echo "<username>\${env.REPO_DEVELOP_USER}</username>";
-      echo "<password>\${env.REPO_DEVELOP_PASSWORD}</password>";
+      echo "<username>\${env.DEPLOY_DEVELOP_USER}</username>";
+      echo "<password>\${env.DEPLOY_DEVELOP_PASSWORD}</password>";
    echo "</server>";
    # Site FTP
    echo "<server>";
       echo "<id>site</id>";
-      echo "<username>\${env.REPO_SITE_USER}</username>";
-      echo "<password>\${env.REPO_SITE_PASSWORD}</password>";
+      echo "<username>\${env.DEPLOY_DOCS_USER}</username>";
+      echo "<password>\${env.DEPLOY_DOCS_PASSWORD}</password>";
    echo "</server>";
    # Development site FTP
    echo "<server>";
       echo "<id>site-development</id>";
-      echo "<username>\${env.REPO_SITE_DEVELOP_USER}</username>";
-      echo "<password>\${env.REPO_SITE_DEVELOP_PASSWORD}</password>";
+      echo "<username>\${env.DEPLOY_DOCS_DEVELOP_USER}</username>";
+      echo "<password>\${env.DEPLOY_DOCS_DEVELOP_PASSWORD}</password>";
    echo "</server>";
    
    echo "</servers>";
    
    # Active profile
-   if [ "$TRAVIS_BRANCH" == "develop" ]; then
+   if [ "$SCM_BRANCH" == "develop" ]; then
       echo "<activeProfiles>"
          echo "<activeProfile>development</activeProfile>"
       echo "</activeProfiles>"
--- .scripts/deploy-site.sh
+++ .scripts/deploy-site.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-# This script deploys the Maven site using the POM configuration
-# It is triggered only commits to the master branch. Pulls are ignored
+# This script deploys the Maven site.
 #
-# Also, it will only deploy if the DEPLOY environment variable is set to 'true'
+# Make sure you have the deployment configuration ready before using it.
+#
+# It is triggered only commits to the master or develop branches, ignoring pulls.
+#
+# Also, it will only deploy if the DEPLOY_DOCS environment variable is set to 'true'.
+#
+# The following environmental variables are used:
+# - PULL_REQUEST: boolean, indicates if this is a pull request, should be false for deployment
+# - DEPLOY_DOCS: boolean, control flag for deployment, should be true to deploy
+# - SCM_BRANCH: string, the CMS branch from which the code has been taken
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$DEPLOY_DOCS" == "true" ] && [[ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "develop" ]]; then
+if [ "$PULL_REQUEST" == "false" ] && [ "$DEPLOY_DOCS" == "true" ] && [[ "$SCM_BRANCH" == "master" || "$SCM_BRANCH" == "develop" ]]; then
 
    echo "Deploying Maven site"
 
