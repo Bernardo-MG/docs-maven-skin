87ae7742a3796e57c0dc8d6e5fd99e0bef149562
Bernardo Martínez Garrido <programming@wandrell.com>
2015-12-07 15:21:57 +0100

    Set a parent POM.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 # This script deploys the project artifact.
 #
-# Make sure you have the deployment configuration ready before using it.
+# Make sure you have the deployment configuration ready before using it, including
+# the environment variables which will indicate if the script is to be run or not.
 #
-# It is triggered only commits to the master or develop branches, ignoring pulls.
+# If everything is correct, the deployment will only occur with release or development
+# versions. And any pull request, in case the code comes from SCM, will be ignored.
 #
-# Also, it will only deploy if the DEPLOY environment variable is set to 'true'.
+# Note that if required the DEPLOY environment variable can be used to stop the script
+# from running by setting it to 'false'.
 #
 # The following environmental variables are used:
-# - PULL_REQUEST: boolean, indicates if this is a pull request, should be false for deployment
 # - DEPLOY: boolean, control flag for deployment, should be true to deploy
-# - SCM_BRANCH: string, the CMS branch from which the code has been taken
+# - PULL_REQUEST: boolean, indicates if this is a pull request, should be false for deployment
+# - VERSION_TYPE: string, the type of version of the code. One of 'release', 'develop' or 'other'.
 
-if [ "$PULL_REQUEST" == "false" ] && [ "$DEPLOY" == "true" ] && [[ "$SCM_BRANCH" == "master" || "$SCM_BRANCH" == "develop" ]]; then
+set -o nounset
+set -e
 
-   echo "Deploying Java artifact to repository"
+if [ "$DEPLOY" == "true" ] && [ "$PULL_REQUEST" == "false" ] && [ "$VERSION_TYPE" != "other" ]; then
 
-   mvn deploy --settings ~/settings.xml
+   echo "Deploying Java artifact"
+
+   mvn deploy -P deployment --settings ~/settings.xml
    
+   exit 0
+      
 else
 
-   echo "Java artifact won't be deployed to repository"
+   echo "Java artifact won't be deployed"
+   
+   exit 0
 
 fi
