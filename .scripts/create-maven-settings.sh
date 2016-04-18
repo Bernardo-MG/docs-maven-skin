8d57de3fc43e5ca6a1b1a89ae2a32c3448808828
Bernardo Martínez Garrido <programming@wandrell.com>
2016-04-18 16:07:04 +0200

    Updated project configuration.

--- .scripts/create-maven-settings.sh
+++ .scripts/create-maven-settings.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# This script creates the Maven settings file for deployment, and is meant to be used
-# as part of the CI process. Said file will be stored on the ~/settings.xml path, and
-# its contents will be created from a series of environmental variables.
+# Creates the Maven settings file for the CI process. Said file will be stored on
+# the ~/settings.xml path, and its contents will be created from a series of
+# environmental variables.
 #
 # The most important information it will contain will be the connection settings for all
 # the repositories used during deployment.
 #
-# For security reasons the data stored in the generated file should not be shared. Never 
+# For security reasons the data stored in the generated file should not be shared. Never
 # print it on the console or let it be accessed in any way.
 #
 # The following environmental variables are required by the script:
@@ -26,12 +26,12 @@ set -e
 
 {
    echo "<settings>";
-   
+
    # ----------------
    # Servers settings
    # ----------------
    echo "<servers>";
-   
+
    # Releases artifacts server
    echo "<server>";
       echo "<id>releases</id>";
@@ -44,7 +44,7 @@ set -e
       echo "<username>\${env.DEPLOY_DOCS_USER}</username>";
       echo "<password>\${env.DEPLOY_DOCS_PASSWORD}</password>";
    echo "</server>";
-   
+
    # Development artifacts server
    echo "<server>";
       echo "<id>snapshots</id>";
@@ -57,16 +57,16 @@ set -e
       echo "<username>\${env.DEPLOY_DOCS_DEVELOP_USER}</username>";
       echo "<password>\${env.DEPLOY_DOCS_DEVELOP_PASSWORD}</password>";
    echo "</server>";
-   
+
    echo "</servers>";
    # ---------------------
    # Ends servers settings
    # ---------------------
-   
+
    # --------------
    # Active profile
    # --------------
-   
+
    # These profiles are used to set the site repository info
    if [ "$VERSION_TYPE" == "develop" ]; then
       # Development version
@@ -79,11 +79,11 @@ set -e
          echo "<activeProfile>deploy-site-release</activeProfile>"
       echo "</activeProfiles>"
    fi
-   
+
    # -------------------
    # Ends active profile
    # -------------------
-   
+
    echo "</settings>";
 } >> ~/settings.xml
 
