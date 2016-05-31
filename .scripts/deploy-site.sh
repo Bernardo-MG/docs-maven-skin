469e885d3c4f90d8890792a5209aaa720befa5ee
Bernardo Martínez Garrido <programming@wandrell.com>
2016-05-31 21:37:01 +0200

    Changed script for verification.

--- .scripts/deploy-site.sh
+++ .scripts/deploy-site.sh
@@ -23,13 +23,7 @@ if [ "$DEPLOY_DOCS" == "true" ] && [ "$PULL_REQUEST" == "false" ] && [ "$VERSION
 
    echo "Deploying Maven site"
 
-   mvn site site:deploy -P deployment --settings ~/settings.xml > site_output.txt
-
-   head -50 site_output.txt
-   echo " "
-   echo "(...)"
-   echo " "
-   tail -50 site_output.txt
+   mvn site site:deploy -P deployment --settings ~/settings.xml
 
    exit 0
 
