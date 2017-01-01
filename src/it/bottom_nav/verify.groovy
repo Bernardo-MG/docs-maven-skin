f4c9e1777a94fd1be1e3bfd27058380cffe5954d
Bernardo Martínez Garrido <programming@wandrell.com>
2017-01-01 20:06:08 +0100

    Added more test cases

--- src/it/bottom_nav/verify.groovy
+++ src/it/bottom_nav/verify.groovy
@@ -8,7 +8,16 @@ assert index.exists()
 // Acquires the sample HTML content
 String html = index.getText()
 
-// Verifies the footer columns exist
+// Verifies the footer columns exist and contain the expected data
 assert html.contains( '<dt>General Info</dt>' )
+assert html.contains( '<dd><a href="./acquire.html" title="Acquire">Acquire</a></dd>' )
+assert html.contains( '<dd><a href="./usage.html" title="Usage">Usage</a></dd>' )
+
 assert html.contains( '<dt>Code</dt>' )
+assert html.contains( '<dd><a href="https://github.com/Bernardo-MG/docs-maven-skin" class="link-noted">SCM' )
+assert html.contains( '<dd><a href="https://travis-ci.org/Bernardo-MG/docs-maven-skin" class="link-noted">CI' )
+assert html.contains( '<a href="https://www.github.com/bernardo-mg/docs-maven-skin/issues" class="link-noted">Issues' )
+
 assert html.contains( '<dt>Releases</dt>' )
+assert html.contains( '<dd><a href="https://bintray.com/bernardo-mg/maven/docs-maven-skin/view">Bintray</a></dd>' )
+assert html.contains( '<dd><a href="http://mvnrepository.com/artifact/com.wandrell.maven.skins/docs-maven-skin">Maven Central</a></dd>' )
