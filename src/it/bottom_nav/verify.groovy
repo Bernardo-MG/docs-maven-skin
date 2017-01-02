ba5fdc683fc065a8f5cfa8c8e09d34f6c5789d9e
Bernardo Martínez Garrido <programming@wandrell.com>
2017-01-02 21:32:25 +0100

    The code menu is now generated automatically also

--- src/it/bottom_nav/verify.groovy
+++ src/it/bottom_nav/verify.groovy
@@ -15,9 +15,9 @@ assert html.contains( '<dd><a href="./acquire.html" title="Acquire">Acquire</a><
 assert html.contains( '<dd><a href="./usage.html" title="Usage">Usage</a></dd>' )
 
 assert html.contains( '<dt>Code</dt>' )
-assert html.contains( '<dd><a href="https://github.com/Bernardo-MG/docs-maven-skin" class="link-noted">SCM' )
-assert html.contains( '<dd><a href="https://travis-ci.org/Bernardo-MG/docs-maven-skin" class="link-noted">CI' )
-assert html.contains( '<a href="https://www.github.com/bernardo-mg/docs-maven-skin/issues" class="link-noted">Issues' )
+assert html.contains( '<dd><a href="https://github.com/Bernardo-MG/docs-maven-skin" title="SCM" class="link-noted">SCM <span class="note">GitHub</span></a></dd>' )
+assert html.contains( '<dd><a href="https://travis-ci.org/Bernardo-MG/docs-maven-skin" title="CI" class="link-noted">CI <span class="note">Travis CI</span></a></dd>' )
+assert html.contains( '<dd><a href="https://www.github.com/bernardo-mg/docs-maven-skin/issues" title="Issues" class="link-noted">Issues <span class="note">GitHub</span></a></dd>' )
 
 assert html.contains( '<dt>Releases</dt>' )
 assert html.contains( '<dd><a href="https://bintray.com/bernardo-mg/maven/docs-maven-skin/view" title="Bintray">Bintray</a></dd>' )
