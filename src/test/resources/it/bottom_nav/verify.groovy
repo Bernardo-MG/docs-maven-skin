374afc7225d582d627ee3b28f72e530b42d48fee
Bernardo <programming@wandrell.com>
2017-03-03 23:10:31 +0100

    More testing done with Jsoup

--- src/test/resources/it/bottom_nav/verify.groovy
+++ src/test/resources/it/bottom_nav/verify.groovy
@@ -37,18 +37,22 @@ MatcherAssert.assertThat(
 // Parses HTML
 def body = Jsoup.parse(html).body()
 
-assert body.select( 'dt' ).size() == 3
+// Verifies the footer columns exist
+def titles = body.select( 'dt' )
+assert titles.size() == 3
+
+// Verifies the footer columns contains the expected titles
+assert titles.get(0).html().equals('General Info')
+assert titles.get(1).html().equals('Code')
+assert titles.get(2).html().equals('Releases')
 
 // Verifies the footer columns exist and contain the expected data
-assert html.contains( '<dt>General Info</dt>' )
 assert html.contains( '<dd><a href="./acquire.html" title="Acquire">Acquire</a></dd>' )
 assert html.contains( '<dd><a href="./usage.html" title="Usage">Usage</a></dd>' )
 
-assert html.contains( '<dt>Code</dt>' )
 assert html.contains( '<dd><a href="https://github.com/Bernardo-MG/docs-maven-skin" title="SCM" class="link-noted">SCM <span class="note">GitHub</span></a></dd>' )
 assert html.contains( '<dd><a href="https://travis-ci.org/Bernardo-MG/docs-maven-skin" title="CI" class="link-noted">CI <span class="note">Travis CI</span></a></dd>' )
 assert html.contains( '<dd><a href="https://www.github.com/bernardo-mg/docs-maven-skin/issues" title="Issues" class="link-noted">Issues <span class="note">GitHub</span></a></dd>' )
 
-assert html.contains( '<dt>Releases</dt>' )
 assert html.contains( '<dd><a href="https://bintray.com/bernardo-mg/maven/docs-maven-skin/view" title="Bintray">Bintray</a></dd>' )
 assert html.contains( '<dd><a href="http://mvnrepository.com/artifact/com.wandrell.maven.skins/docs-maven-skin" title="Maven Central">Maven Central</a></dd>' )
