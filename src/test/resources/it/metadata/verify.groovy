36109e5c84b1d2d06bc57dc74b8d28c9ddb853bc
Bernardo <programming@wandrell.com>
2017-04-18 22:34:36 +0200

    Prepared metadata test

---
+++ src/test/resources/it/metadata/verify.groovy
@@ -0,0 +1,71 @@
+// This script verifies that a site with only metadata set up contains the correct metadata.
+
+import com.jcabi.w3c.ValidatorBuilder
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.jsoup.Jsoup
+
+// Verifies that all the files were created
+[
+    'target/site/index.html'
+].each {
+    def file = new File(basedir, it)
+    if (!file.exists()) {
+        throw new IllegalStateException(
+            "file ${file} doesn't exist"
+        )
+    }
+}
+
+// Acquires the sample HTML content
+def html = new File(basedir, 'target/site/index.html').text
+
+// Validates HTML 5
+def htmlResponse = new ValidatorBuilder().html().validate(html)
+
+MatcherAssert.assertThat(
+    'There are errors',
+    htmlResponse.errors(),
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
+)
+MatcherAssert.assertThat(
+    'There are warnings',
+    htmlResponse.warnings(),
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
+)
+
+// Parses HTML
+def head = Jsoup.parse(html).head()
+
+// Verifies the basic metadata is generated
+def metaDesc = head.select( 'meta[name="description"]' )
+def metaKeywords = head.select( 'meta[name="keywords"]' )
+def metaAuthor = head.select( 'meta[name="author"]' )
+
+assert metaDesc.attr( 'content' ).equals( 'Check the documentation for the example' )
+assert metaKeywords.attr( 'content' ).equals( 'Maven, Java, library' )
+assert metaAuthor.attr( 'content' ).equals( 'Bernardo Martínez Garrido' )
+
+// Verifies the Facebook Open Graph metadata is generated
+def metaOgType = head.select( 'meta[name="og:type"]' )
+def metaOgUrl = head.select( 'meta[name="og:url"]' )
+def metaOgSite = head.select( 'meta[name="og:site"]' )
+def metaOgTitle = head.select( 'meta[name="og:title"]' )
+def metaOgDesc = head.select( 'meta[name="og:description"]' )
+
+assert metaOgType.attr( 'content' ).equals( 'website' )
+assert metaOgUrl.attr( 'content' ).equals( 'http://canonicallink' )
+assert metaOgSite.attr( 'content' ).equals( 'metadata-site &#x2013; Metadata page' )
+assert metaOgTitle.attr( 'content' ).equals( 'metadata-site &#x2013; Metadata page' )
+assert metaOgDesc.attr( 'content' ).equals( 'Check the documentation for the example' )
+
+// Verifies the Twitter metadata is generated
+def metaTwCard = head.select( 'meta[name="twitter:card"]' )
+def metaTwSite = head.select( 'meta[name="twitter:site"]' )
+def metaTwTitle = head.select( 'meta[name="twitter:title"]' )
+def metaTwDesc = head.select( 'meta[name="twitter:description"]' )
+
+assert metaTwCard.attr( 'content' ).equals( 'summary' )
+assert metaTwSite.attr( 'content' ).equals( '@bmg' )
+assert metaTwTitle.attr( 'content' ).equals( 'metadata-site &#x2013; Metadata page' )
+assert metaTwDesc.attr( 'content' ).equals( 'Check the documentation for the example' )
