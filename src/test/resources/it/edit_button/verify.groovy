7d4a4439142250afb2fb18d024af0328f48977bd
Bernardo <programming@wandrell.com>
2018-07-31 22:48:54 +0200

    Corrected tested link

--- src/test/resources/it/edit_button/verify.groovy
+++ src/test/resources/it/edit_button/verify.groovy
@@ -44,5 +44,5 @@ def body = Jsoup.parse(html).body()
 
 // Verifies the edition link was created
 def edit = body.select( 'a > span.fa-edit' ).first().parent()
-assert edit.attr( 'href' ).equals( 'https://github.com/Bernardo-MG/docs-maven-skin/src\\site\\markdown/index.md' )
+assert edit.attr( 'href' ).equals( 'https://github.com/Bernardo-MG/docs-maven-skin/src/site/markdown/index.md' )
 
