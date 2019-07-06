0d350555f21800fad5c010b704411e957bcc3751
Bernardo <programming@wandrell.com>
2019-07-07 01:02:42 +0200

    Added tests for images

---
+++ src/test/resources/it/image/verify.groovy
@@ -0,0 +1,49 @@
+// This script verifies that a minimal site contains only the barebones of a site.
+
+import com.jcabi.w3c.ValidatorBuilder
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.jsoup.Jsoup
+
+// Verifies that all the files were created
+[
+    'target/site/index.html',
+    'target/site/favicon.ico',
+    'target/site/robots.txt',
+    'target/site/css/style.min.css',
+    'target/site/js/scripts.min.js'
+].each {
+    def file = new File(basedir, it)
+    if (!file.exists()) {
+        throw new IllegalStateException(
+            "file ${file} doesn't exist"
+        )
+    }
+}
+
+// Acquires the sample HTML content
+def html = new File(basedir, 'target/site/index.html').text
+
+// Validates HTML 5
+def htmlResponse = new ValidatorBuilder().html().validate(html)
+
+MatcherAssert.assertThat(
+    'There are errors',
+    htmlResponse.errors(),
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
+)
+MatcherAssert.assertThat(
+    'There are warnings',
+    htmlResponse.warnings(),
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
+)
+
+// Parses HTML
+def parsed = Jsoup.parse(html)
+def body = parsed.body()
+
+// Images
+def imgs = body.select( 'img' )
+
+def firstImg = imgs.first()
+assert firstImg.attr( 'alt' ).contains( 'Dice class diagram' )
