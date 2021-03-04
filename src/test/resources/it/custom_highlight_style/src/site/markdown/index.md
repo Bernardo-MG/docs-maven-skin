8bad861ff431992ad50f13506bf1b5fe41299556
Collin Brooks <cbrooks@sentryds.com>
2021-03-04 12:33:56 -0600

    [New Feature] Custom highlight.js style
     - Added the ability to specify the name of a highlight.js style css
       file. If not provided, the default is used.
     - Added tests to confirm both default and custom highlight.js styles.
     - Updated documentation with new custom highlight config option.

---
+++ src/test/resources/it/custom_highlight_style/src/site/markdown/index.md
@@ -0,0 +1,3 @@
+# Custom Highlight Style
+
+This is an example for a Maven Site with a custom highlight.js style.
---
+++ src/test/resources/it/default_highlight_style/verify.groovy
@@ -0,0 +1,32 @@
+// This script verifies that the default highlight.js style is applied when no customHighlightStyle
+// is provided.
+
+import com.jcabi.w3c.ValidatorBuilder
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.jsoup.Jsoup
+
+// Acquires the sample HTML content
+def html = new File(basedir, 'target/site/index.html').text
+
+// Validates HTML 5
+def htmlResponse = new ValidatorBuilder().html().validate(html)
+
+MatcherAssert.assertThat(
+    'There are errors',
+    htmlResponse.errors(),
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
+)
+MatcherAssert.assertThat(
+    'There are warnings',
+    htmlResponse.warnings(),
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
+)
+
+// Parses HTML
+def parsed = Jsoup.parse(html)
+def head = parsed.head()
+
+// The default highlight style is applied
+def customStyle = head.select( 'link[href="./lib/highlight/styles/default.css"]' )
+assert customStyle.outerHtml().equals('<link rel="stylesheet" href="./lib/highlight/styles/default.css">')
