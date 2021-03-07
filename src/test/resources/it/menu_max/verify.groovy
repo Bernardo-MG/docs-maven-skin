27f02059663eedd9d7f328feb7ccf1c8042f35fd
Bernardo <programming@bernardomg.com>
2021-03-07 12:15:40 +0100

    Added test for a big menu

---
+++ src/test/resources/it/menu_max/verify.groovy
@@ -0,0 +1,35 @@
+// This script verifies that a minimal site contains only the barebones of a site.
+
+import com.jcabi.w3c.ValidatorBuilder
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.jsoup.Jsoup
+
+// Acquires the sample HTML content
+def html = new File(basedir, 'target/site/index.html').text
+
+// Validates HTML 5
+def htmlResponse = new ValidatorBuilder().html().validate(html)
+
+MatcherAssert.assertThat(
+    'There are errors',
+    htmlResponse.errors(),
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
+)
+MatcherAssert.assertThat(
+    'There are warnings',
+    htmlResponse.warnings(),
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
+)
+
+// Parses HTML
+def body = Jsoup.parse(html).body()
+
+// Verifies the menus exist
+def toggles = body.select( '#navbar-main-menu .dropdown-toggle' )
+assert toggles.size() == 12
+
+// Verifies the menu links exist
+def dropdowns = body.select( '#navbar-main-menu .dropdown' )
+def items = dropdowns.get(0).select( '.dropdown-item' )
+assert items.size() == 10
