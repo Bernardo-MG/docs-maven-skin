ff8b4d6f736a1b0d8e806939770f9707f25e71a3
Bernardo <programming@bernardomg.com>
2021-03-08 21:54:38 +0100

    Added tests

---
+++ src/test/resources/it/custom_bootswatch_style/verify.groovy
@@ -0,0 +1,31 @@
+// This script verifies the Bootswatch style can be changed
+
+import com.jcabi.w3c.ValidatorBuilder
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.jsoup.Jsoup
+
+// Acquires the sample HTML content
+def html = new File(basedir, 'target/site/index.html').text
+
+// Validates HTML 5
+def htmlResponse = new ValidatorBuilder().html().validate(html)
+
+MatcherAssert.assertThat(
+    'There are errors',
+    htmlResponse.errors(),
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
+)
+MatcherAssert.assertThat(
+    'There are warnings',
+    htmlResponse.warnings(),
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
+)
+
+// Parses HTML
+def parsed = Jsoup.parse(html)
+def head = parsed.head()
+
+// The custom highlight style is applied
+def customStyle = head.select( 'link[href="./lib/bootswatch/dist/sketchy/bootstrap.min.css"]' )
+assert customStyle.outerHtml().equals('<link rel="stylesheet" href="./lib/bootswatch/dist/sketchy/bootstrap.min.css">')
