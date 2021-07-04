5f2657da1eb7d06ea810c9387523697ec33e5ff9
Bernardo <programming@bernardomg.com>
2021-07-04 15:53:42 +0200

    Added tests

---
+++ src/test/resources/it/custom_shine_dark_default/verify.groovy
@@ -0,0 +1,14 @@
+// This script verifies the Bootswatch style can be changed
+
+import org.jsoup.Jsoup
+
+// Acquires the sample HTML content
+def html = new File(basedir, 'target/site/index.html').text
+
+// Parses HTML
+def body = Jsoup.parse(html).body()
+
+// The dark style is applied
+def nav = body.select( '#navbar-main-menu' )
+assert nav.hasClass( 'navbar-dark' )
+assert nav.hasClass( 'bg-primary' )
