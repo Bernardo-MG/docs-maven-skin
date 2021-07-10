30a5793c4ed39ad9372d019be7195995ae4db031
Bernardo <programming@bernardomg.com>
2021-07-10 22:00:36 +0200

    Removed HTML5 validation to speed up test

--- src/test/resources/it/image_shine_dark/verify.groovy
+++ src/test/resources/it/image_shine_dark/verify.groovy
@@ -1,26 +1,9 @@
 // This script verifies that a minimal site contains only the barebones of a site.
 
-import com.jcabi.w3c.ValidatorBuilder
-import org.hamcrest.MatcherAssert
-import org.hamcrest.Matchers
 import org.jsoup.Jsoup
 
 // Acquires the sample HTML content
 def html = new File(basedir, 'target/site/index.html').text
-
-// Validates HTML 5
-def htmlResponse = new ValidatorBuilder().html().validate(html)
-
-MatcherAssert.assertThat(
-    'There are errors',
-    htmlResponse.errors(),
-    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
-)
-MatcherAssert.assertThat(
-    'There are warnings',
-    htmlResponse.warnings(),
-    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
-)
 
 // Parses HTML
 def parsed = Jsoup.parse(html)
