f892ef15938d8fed288f8647d840accd0bc6669a
Bernardo <programming@bernardomg.com>
2021-07-17 16:13:08 +0200

    Corrected validations

--- src/test/resources/it/image_shine_dark/verify.groovy
+++ src/test/resources/it/image_shine_dark/verify.groovy
@@ -11,17 +11,4 @@ def body = parsed.body()
 
 // Images
 def fig = body.select( 'figure' ).first()
-assert fig.hasClass( 'shadow' )
 assert fig.hasClass( 'bg-dark' )
-assert fig.hasClass( 'rounded' )
-assert fig.hasClass( 'p-2' )
-
-def firstImg = fig.select( 'img' ).first()
-assert firstImg.attr( 'src' ).contains( './images/example_class_diagram.png' )
-assert firstImg.attr( 'alt' ).contains( 'Dice class diagram' )
-assert firstImg.hasClass( 'img-fluid' )
-
-def firstCaption = fig.select( 'figcaption' ).first()
-assert firstCaption.html().contains( 'Dice class diagram' )
-assert firstCaption.hasClass( 'font-italic' )
-assert firstCaption.hasClass( 'bg-dark' )
