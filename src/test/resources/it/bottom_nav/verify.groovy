d314ba179d94747d75f6e11d39825230d8d9f0d6
Bernardo-MG <programming@bernardomg.com>
2024-09-10 00:57:38 +0200

    Comparing against text

--- src/test/resources/it/bottom_nav/verify.groovy
+++ src/test/resources/it/bottom_nav/verify.groovy
@@ -15,7 +15,7 @@ def body = Jsoup.parse(html).body()
 def footerInfo = body.select( '#footer-info' )
 
 def copyrightInfo = footerInfo.select( 'div' ).first()
-assert copyrightInfo.html().contains( 'Bernardo Martínez Garrido - MIT License' )
+assert copyrightInfo.text().contains( 'Bernardo Martínez Garrido - MIT License' )
 
 def copyrightIcon = copyrightInfo.select( '.fa-copyright' )
 assert copyrightIcon.size() == 1
