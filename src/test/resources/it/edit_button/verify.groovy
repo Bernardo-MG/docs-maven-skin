67aebd203b8ee270a1b8b110c1261afad3c451d7
Bernardo-MG <programming@bernardomg.com>
2025-06-07 14:36:45 +0200

    Corrected test

--- src/test/resources/it/edit_button/verify.groovy
+++ src/test/resources/it/edit_button/verify.groovy
@@ -7,7 +7,9 @@ def html = new File(basedir, 'target/site/index.html').text
 def body = Jsoup.parse(html).body()
 
 // Verifies the edition link was created
-assert !body.select('a > span.fa-pen-to-square').isEmpty()
+def editIcons = body.select('a > span.fa-pen-to-square')
+assert !editIcons.isEmpty()
+def editIcon = icons.first()
 
 def editLink = editIcon.parent()
 assert editLink.attr('href') == 'https://github.com/Bernardo-MG/docs-maven-skin/src/site/markdown/index.md'
